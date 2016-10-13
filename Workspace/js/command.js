8ff30e182fbb8268929a402d3f8fd8347e925d2b
Admin <admin@Admins-MacBook-Pro.local>
2016-10-12 21:27:05 -0700

    debug 1

--- Workspace/js/command.js
+++ Workspace/js/command.js
@@ -1,4 +1,5 @@
 var helps={
+	//DEL, CREATION COMMANDS, MANAGE COMMANDS, 
 	help1:"PROTO ECLIPSE HELP MENU: ",
 	help2:"COMMANDS ARE DISPLAYED WITHIN PARENTHESISES: (command)",
 	help7:"(del name) ENTER THE FOLDER YOU WISH TO DELETE IN NAME",
@@ -9,8 +10,13 @@ var helps={
 	lol:"(amanage action name value) TO MANAGE ARRAY; ACTIONS ARE PUSH, SHIFT, UNSHIFT, POP. VALUE IS NEEDED FOR UNSHIFT AND PUSH",
 	help10:"(help) OPENS THIS MESSAGE",
 	doesntMatter:"(view) PRINTS ALL DIRECTORY WITHIN WORKSPACE",
-	clear:"(clear) CLEARS WORKSPACE"
+	clear:"(clear) CLEARS WORKSPACE",
+	a:"(lorem) LOADS LOREM IPSUM",
+	wea:"(weather) LOADS CURRENT WEATHER IN YOUR LOCATION",
+	reset:"(refresh) REFRESHES THE PAGE",
+	cd:"(cd path) CHANGES CURRENT DIRECTORY REPLACE PATH WITH THE ENTIRE PATH TO THE DIRECTORY YOU WISH TO GO. EX: 'HOME APPLICATIONS VIDEOS'"
 }
+var lorem="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Lorem ipsum dolor sit amet, purus nam, auctor maecenas mus vitae a aliquet, dictumst eget, molestie in et magna, convallis non. At pede aliquet <br>interdum cras, sed volutpat enim pede amet, quisque donec mus mauris arcu, eu tempus, wisi ut cursus neque rhoncus in. Nulla dui <br>lorem semper ante. Purus amet porta elit id, nibh fusce, semper eget commodo suspendisse, mauris leo nec sed sem potenti. Vel mi, egestas blandit <br>pellentesque varius ante. Sed morbi et metus est nunc aliquam, eget orci tempus cum convallis quis. Neque adipiscing class et donec magna velit, <br>feugiat sem amet quam ac ac aut, augue viverra morbi consectetuer. Sit laborum sed sed, sapien integer sit, urna praesent, a libero nec vitae <br>laoreet mauris. Est aliquam, morbi tortor wisi tellus amet, ea amet accumsan lobortis cras mi, dui congue dolor semper sed at ipsum, velit tortor <br>donec adipiscing justo. Aliquet dolor faucibus id. <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Parturient sed, cras elit libero, ultricies ut sed mollis, venenatis magna ac platea tincidunt posuere a. Quam excepturi vel hendrerit,<br> quis mi cras vivamus, nec quam mauris libero vel odio, luctus proin dui penatibus enim pede suspendisse, <br>vitae ante ut. Tellus pulvinar sollicitudin dapibus. Officiis elementum ipsum tincidunt facilisis, vel odio, id integer leo. Nulla pulvinar neque <br>vel, libero scelerisque, nibh integer tincidunt per, orci quis tempor arcu, cras suspendisse nam donec. Dui ultrices sed wisi quam erat lacus. <br>Aenean dui bibendum, quis felis sit vel, eros dui nec, eget pretium non ac. Tellus gravida, quis porta ut nulla at vel, quis donec id, nunc <br>curabitur ullamcorper lectus eu consequat, integer at accumsan mi. In aliquam fusce, erat porta in in risus et, erat id adipiscing inceptos <br>adipiscing velit, in arcu nec sem wisi. Nec laoreet erat, ut suscipit proin arcu feugiat ante, non sit placerat. Suscipit a ipsum, taciti justo <br>ante dui, habitant mauris, non ut nibh ipsum curabitur, sollicitudin neque nascetur fermentum dignissim. <br>Augue wisi arcu nulla. Sed et tortor <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;vitae, pretium dui tempor amet ullamcorper amet euismod, in massa congue urna fusce, quis cubilia ultrices lobortis, felis fusce. Platea rutrum <br> pulvinar a, interdum non quam suspendisse neque, per amet fringilla sem magna litora. Quis lacus nulla nec consectetuer sapien lectus, congue donec. <br>Malesuada elit mi placerat magna turpis. Praesent scelerisque quis pretium sociis, fusce luctus. Similique ultricies faucibus feugiat, pede duis, <br>venenatis magna. Vestibulum dignissim nulla suscipit ac ullamcorper. Vestibulum voluptatem ligula, ac integer feugiat, lectus at magna, dolore <br>pretium molestie pulvinar. Erat a proin. <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Luctus ac in, elit esse, ipsam sed mollis phasellus dictum. Nec vitae. Cras purus praesent porttitor a dolor natoque, urna amet velit sed <br>adipiscing. Sit arcu metus magna, viverra dolor, fames iaculis praesent congue sagittis. Pulvinar dis purus <br>lobortis mauris lectus, nulla rutrum volutpat tempor ut quam interdum, senectus ultrices vestibulum massa, aliquam ridiculus et vitae pede dui <br>vestibulum, eu possimus ante ac. Nec lacus mi ut ullamcorper consectetuer, mattis faucibus, nec quis ea quisque sed hendrerit. Ipsam eu feugiat <br>ipsum, ligula nulla, metus mollis faucibus lacinia, ligula commodo lobortis in, rem sed quis dignissim rutrum mi dapibus. Leo etiam, dui metus <br>placerat faucibus ut, potenti arcu ultricies, tempus donec et iaculis, platea justo amet habitant. Egestas tincidunt, aliquet facilisi porttitor <br>dignissim magnis.";
 var input;
 var error="Command Not Found"
 var one, two, three, four, five;
@@ -23,7 +29,7 @@ function main (item) {
 				case "del":
 					feed="del";
 					break;
-				case "clear":
+				case "clearµµµ":
 					feed="clear";
 					break;
 				case "gmanage":
@@ -46,6 +52,18 @@ function main (item) {
 					break;
 				case "string":
 					feed="sC";
+					break;
+				case "lorem":
+					feed="lorem";
+					break;
+				case "weather":
+					feed="weather";
+					break;
+				case "refresh":
+					feed="refresh";
+					break;
+				case "cd":
+					feed="cd";
 					break;
 			}
 		}
@@ -97,13 +115,27 @@ function main (item) {
 			two=two.join(" ");
 			sc(one,two);
 			break;
+		case "lorem":
+			loremF();
+			break;
+		case "weather":
+			weather();
+			break;
+		case "refresh":
+			ref();
+			break;
+		case "cd":
+			one=arryr.slice(1,arr.length);
+			cd(one);
+			break;
 		default:
 			$("#targetDiv").prepend('<pre>'+error+'</pre>');
 			break;
 	}
 };
 function del (folder) {
-	if(computer[folder]){
+	var folders=folder;
+	if(computer[folders]){
 		delete computer[folder];
 	}else{
 		$("#targetDiv").prepend("<pre>Folder Not Found</pre>");
@@ -175,7 +207,13 @@ function oc (newFileName,propertyName, value) {
 		$("#targetDiv").prepend("<pre>Missing Information</pre>");
 	}
 	if(!computer[newFile]){
-		computer[newFile]={[property]:values};
+		if(!property){
+			computer[newFile]={};
+		}else if(!values){
+			computer[newFile]={[property]:null};
+		}else{
+			computer[newFile]={[property]:values};
+		}
 		$("#targetDiv").prepend("<pre>File Created</pre>");
 	}else{
 		$("#targetDiv").prepend("<pre>File Name Exists</pre>");
@@ -211,4 +249,51 @@ function sc (names, value) {
 		$("#targetDiv").prepend("<pre>File Name Exists</pre>");
 	}
 	$('#targetDiv').scrollTop($('#div1').height());
+}
+function loremF () {
+	$("#targetDiv").prepend("<pre>"+lorem+"</pre>");
+	$('#targetDiv').scrollTop($('#div1').height());
+}
+function weather () {
+	var unit = "metric";
+    $.getJSON("http://ip-api.com/json", function(json){
+	    var city, country, lat, lon, apiId;
+	    city = json.city;
+	    country = json.country;
+	    $("#th").html("Location: "+city+", "+country);
+	    lat = json.lat;
+	    lon = json.lon;
+	    apiId = "12ae4ce34fa575032f23a9b8b490e9a9";
+	    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID="+apiId+"&units="+unit, function(weatherData ){
+		    var self, weather, wind, clouds,description, temp, pressure, humidity, temp_min, temp_max,windeg;
+		    self=weatherData;
+		    weather = self.weather[0].main;
+		    description = self.weather[0].description;
+		    wind=self.wind.speed;
+		    windeg=self.wind.deg;
+		    clouds=self.clouds.all;
+		    temp=self.main.temp;
+		    pressure=self.main.pressure;
+		    humidity=self.main.humidity;
+		    temp_min=self.main.temp_min;
+		    temp_max=self.main.temp_max;
+		    $("#targetDiv").prepend('<pre> Wind: '+wind+' kph</pre>');
+		    $("#targetDiv").prepend("<pre> Wind Degree: "+windeg+"&deg;</pre>");
+		    $("#targetDiv").prepend("<pre> Temperature: "+temp+"&deg; fahrenheit</pre>");
+		    $("#targetDiv").prepend("<pre> Air Pressure: "+pressure+" mbar</pre>");
+		    $("#targetDiv").prepend("<pre> Humidity: "+humidity+" kg</pre>");
+		    $("#targetDiv").prepend("<pre> Max Temperature Today: "+temp_min+"&deg; fahrenheit</pre>");
+		    $("#targetDiv").prepend("<pre> Min Temperature Today: "+temp_max+"&deg; fahrenheit</pre>");
+		    $("#targetDiv").prepend("<pre> Clouds: "+clouds+"%</pre>");
+		    $("#targetDiv").prepend("<pre> Weather: "+weather+"</pre>");
+		    $("#targetDiv").prepend("<pre> Weather Description: "+description+"</pre>");
+		    $('#targetDiv').scrollTop($('#div1').height());
+	  	});
+	});
+}
+function ref () {
+	location.reload();
+}
+function cd(arr){
+
 }
--- Workspace/js/personnel.js
+++ Workspace/js/personnel.js
@@ -1,217 +1,214 @@
-var personnel=[
-  {"Administrating Sector":[
+var personnel={
+  "Administrating Sector":[
       {"chairman":
-        {"firstName":"", "lastName":"", "id":"000001"}
+        {"firstName":"Richard", "lastName":"Harmon", "id":"000001"}
 
       },{"president":
-        {"firstName":"", "lastName":"", "id":"000002"}
+        {"firstName":"Michael", "lastName":"Millerson", "id":"000002"}
 
       },{"vice chairman":
-        {"firstName":"", "lastName":"", "id":"000003"}
+        {"firstName":"Robert", "lastName":"Ford", "id":"000003"}
 
       },{"CEO":
-        {"firstName":"", "lastName":"", "id":"000004"}
+        {"firstName":"Emmitt", "lastName":"Austin", "id":"000004"}
 
       },{"IT director":
-        {"firstName":"", "lastName":"", "id":"000005"}
+        {"firstName":"Justin", "lastName":"Huebert", "id":"000005"}
 
       },{"chief technical officer":
-        {"firstName":"", "lastName":"", "id":"000006"}
+        {"firstName":"Alissa", "lastName":"Kenn", "id":"000006"}
 
       },{"MIS director":
-        {"firstName":"", "lastName":"", "id":"000007"}
+        {"firstName":"David", "lastName":"Graham", "id":"000007"}
 
       },{"chief information officer":
-        {"firstName":"", "lastName":"", "id":"000008"}
+        {"firstName":"Clinton", "lastName":"Smith", "id":"000008"}
 
       },{"vice president":
-        {"firstName":"", "lastName":"", "id":"000009"}
+        {"firstName":"Barbara", "lastName":"Hernandez", "id":"000009"}
 
       },{"senior manager":
-        {"firstName":"", "lastName":"", "id":"000010"}
+        {"firstName":"Elvira", "lastName":"Keenan", "id":"000010"}
 
       },{"administration head":
-        {"firstName":"", "lastName":"", "id":"000011"}
+        {"firstName":"Nadzieja", "lastName":"Sherwood", "id":"000011"}
       }
-    ]
-  },
-  {"Executive Sector":[
+    ],
+  "Executive Sector":[
       {"IT manager":
       [
-        {"firstName":"", "lastName":"", "id":"000012"},
-        {"firstName":"", "lastName":"", "id":"000013"}
+        {"id":"000012"},
+        {"id":"000013"}
       ]
       },{"hardware systems manager":
       [
-        {"firstName":"", "lastName":"", "id":"000014"},
-        {"firstName":"", "lastName":"", "id":"000015"}
+        {"id":"000014"},
+        {"id":"000015"}
       ]
       },{"head of programming department":
       [
-        {"firstName":"", "lastName":"", "id":"000016"},
-        {"firstName":"", "lastName":"", "id":"000017"}
+        {"id":"000016"},
+        {"id":"000017"}
       ]
       },{"chief marketing officer":
       [
-        {"firstName":"", "lastName":"", "id":"000018"},
-        {"firstName":"", "lastName":"", "id":"000019"}
+        {"id":"000018"},
+        {"id":"000019"}
       ]
       },{"senior technology engineer":
       [
-        {"firstName":"", "lastName":"", "id":"000020"},
-        {"firstName":"", "lastName":"", "id":"000021"}
+        {"id":"000020"},
+        {"id":"000021"}
       ]
       },{"chief sales officer":
       [
-        {"firstName":"", "lastName":"", "id":"000022"},
-        {"firstName":"", "lastName":"", "id":"000023"}
+        {"id":"000022"},
+        {"id":"000023"}
       ]
       },{"head of web technologies":
       [
-        {"firstName":"", "lastName":"", "id":"000024"},
-        {"firstName":"", "lastName":"", "id":"000025"}
+        {"id":"000024"},
+        {"id":"000025"}
       ]
       },{"business analyst":
       [
-        {"firstName":"", "lastName":"", "id":"000026"},
-        {"firstName":"", "lastName":"", "id":"000027"}
+        {"id":"000026"},
+        {"id":"000027"}
       ]
       },{"auditor":
       [
-        {"firstName":"", "lastName":"", "id":"000028"},
-        {"firstName":"", "lastName":"", "id":"000029"}
+        {"id":"000028"},
+        {"id":"000029"}
       ]
       },{"chief finance officer":
       [
-        {"firstName":"", "lastName":"", "id":"000030"},
-        {"firstName":"", "lastName":"", "id":"000031"}
+        {"id":"000030"},
+        {"id":"000031"}
       ]
       },{"project manager":
       [
-        {"firstName":"", "lastName":"", "id":"000032"},
-        {"firstName":"", "lastName":"", "id":"000033"}
+        {"id":"000032"},
+        {"id":"000033"}
       ]
       },{"head of creative ventures":
       [
-        {"firstName":"", "lastName":"", "id":"000034"},
-        {"firstName":"", "lastName":"", "id":"000035"}
+        {"id":"000034"},
+        {"id":"000035"}
       ]
       },{"head of engineering department":
       [
-        {"firstName":"", "lastName":"", "id":"000036"},
-        {"firstName":"", "lastName":"", "id":"000037"}
+        {"id":"000036"},
+        {"id":"000037"}
       ]
       },{"senior database administrator":
       [
-        {"firstName":"", "lastName":"", "id":"000038"},
-        {"firstName":"", "lastName":"", "id":"000039"}
+        {"id":"000038"},
+        {"id":"000039"}
       ]
       },{"telecommunications manager":
       [
-        {"firstName":"", "lastName":"", "id":"000040"},
-        {"firstName":"", "lastName":"", "id":"000041"}
+        {"id":"000040"},
+        {"id":"000041"}
       ]
       },{"project leader":
       [
-        {"firstName":"", "lastName":"", "id":"000042"},
-        {"firstName":"", "lastName":"", "id":"000043"}
+        {"id":"000042"},
+        {"id":"000043"}
       ]
       },{"networking administrator":
       [
-        {"firstName":"", "lastName":"", "id":"000044"},
-        {"firstName":"", "lastName":"", "id":"000045"}
+        {"id":"000044"},
+        {"id":"000045"}
       ]
       }
-    ]
-  },
-  {"Operational Sector":[
+    ],
+  "Operational Sector":[
       {"database commissioner":
       [
-        {"firstName":"", "lastName":"", "id":"000046"},
-        {"firstName":"", "lastName":"", "id":"000047"}
+        {"id":"000046"},
+        {"id":"000047"}
       ]
       },{"application engineer":
       [
-        {"firstName":"", "lastName":"", "id":"000048"},
-        {"firstName":"", "lastName":"", "id":"000049"}
+        {"id":"000048"},
+        {"id":"000049"}
       ]
       },{"networking engineer":
       [
-        {"firstName":"", "lastName":"", "id":"000050"},
-        {"firstName":"", "lastName":"", "id":"000051"}
+        {"id":"000050"},
+        {"id":"000051"}
       ]
       },{"network administrator":
       [
-        {"firstName":"", "lastName":"", "id":"000052"},
-        {"firstName":"", "lastName":"", "id":"000053"}
+        {"id":"000052"},
+        {"id":"000053"}
       ]
       },{"help desk technician":
       [
-        {"firstName":"", "lastName":"", "id":"000054"},
-        {"firstName":"", "lastName":"", "id":"000055"}
+        {"id":"000054"},
+        {"id":"000055"}
       ]
       },{"system administrator":
       [
-        {"firstName":"", "lastName":"", "id":"000056"},
-        {"firstName":"", "lastName":"", "id":"000057"}
+        {"id":"000056"},
+        {"id":"000057"}
       ]
       },{"security specialist":
       [
-        {"firstName":"", "lastName":"", "id":"000058"},
-        {"firstName":"", "lastName":"", "id":"000059"}
+        {"id":"000058"},
+        {"id":"000059"}
             ] 
       },{"software programmer":
       [
-        {"firstName":"", "lastName":"", "id":"000060"},
-        {"firstName":"", "lastName":"", "id":"000061"}
+        {"id":"000060"},
+        {"id":"000061"}
       ]
       },{"telecommunications engineer":
       [
-        {"firstName":"", "lastName":"", "id":"000062"},
-        {"firstName":"", "lastName":"", "id":"000063"}
+        {"id":"000062"},
+        {"id":"000063"}
       ]
       },{"head of customer representative team":
       [
-        {"firstName":"", "lastName":"", "id":"000064"},
-        {"firstName":"", "lastName":"", "id":"000065"}
+        {"id":"000064"},
+        {"id":"000065"}
       ] 
       },{"head of content writing team":
       [
-        {"firstName":"", "lastName":"", "id":"000066"},
-        {"firstName":"", "lastName":"", "id":"000067"}
+        {"id":"000066"},
+        {"id":"000067"}
       ]
       },{"headware engineer":
       [
-        {"firstName":"", "lastName":"", "id":"000068"},
-        {"firstName":"", "lastName":"", "id":"000069"}
+        {"id":"000068"},
+        {"id":"000069"}
       ]
       },{"software tetsing engineer":
       [
-        {"firstName":"", "lastName":"", "id":"000070"},
-        {"firstName":"", "lastName":"", "id":"000071"}
+        {"id":"000070"},
+        {"id":"000071"}
             ] 
       },{"head of technician team":
       [
-        {"firstName":"", "lastName":"", "id":"000072"},
-        {"firstName":"", "lastName":"", "id":"000073"}
+        {"id":"000072"},
+        {"id":"000073"}
       ]
       },{"technology analyst":
       [
-        {"firstName":"", "lastName":"", "id":"000074"},
-        {"firstName":"", "lastName":"", "id":"000075"}
+        {"id":"000074"},
+        {"id":"000075"}
       ]
       },{"technology associate":
       [
-        {"firstName":"", "lastName":"", "id":"000076"},
-        {"firstName":"", "lastName":"", "id":"000077"}
+        {"id":"000076"},
+        {"id":"000077"}
       ]
       },{"trainee":
       [
-        {"firstName":"", "lastName":"", "id":"000078"},
-        {"firstName":"", "lastName":"", "id":"000079"},
-        {"firstName":"", "lastName":"", "id":"000080"}
+        {"id":"000078"},
+        {"id":"000079"},
+        {"id":"000080"}
       ]
       }
     ]
   }
-]
